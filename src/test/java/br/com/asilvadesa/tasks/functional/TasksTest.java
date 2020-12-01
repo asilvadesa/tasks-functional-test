5e3fedd667ccd85757b9ec9034d39fa86b9de333
Anderson de Sa <asilvadesa@gmail.com>
2020-11-30 20:54:28 -0400

    Add first test case save tasks with sucess

---
+++ src/test/java/br/com/asilvadesa/tasks/functional/TasksTest.java
@@ -0,0 +1,39 @@
+package br.com.asilvadesa.tasks.functional;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TasksTest {
+
+    private WebDriver getWebDriver() {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8001/tasks/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return driver;
+    }
+
+
+    @Test
+    public void deveSalvarTarefaComSucesso(){
+        WebDriver driver = getWebDriver();
+        try{
+
+            driver.findElement(By.id("addTodo")).click();
+            driver.findElement(By.id("task")).sendKeys("Test by Selennium");
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2030");
+            driver.findElement(By.id("saveButton")).click();
+            String message = driver.findElement(By.id("message")).getText();
+
+            Assert.assertEquals("Success!", message);
+        } finally {
+            driver.quit();
+        }
+    }
+
+}
